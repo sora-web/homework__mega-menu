84834bd0e2ce67ed7b84c25ffc7b62a2e9f4939f
sora-web <brave27yuto@gmail.com>
2022-09-16 20:09:13 +0900

    作成 header and 修正 top-page

--- js/script.js
+++ js/script.js
@@ -1,26 +1,12 @@
-// //burger
-// $(".p-header__burger").on("click", function () {
-//   $("body").toggleClass("is__fixed");
-//   $(".p-header__burger").toggleClass("is__active");
-//   $(".p-burger-view").toggleClass("is__open");
-// });
+//burger
+$(".p-header__burger").on("click", function () {
+  $("body").toggleClass("is__fixed");
+  // $(".p-header__burger").toggleClass("is__active");
+  $(".p-burger-view").toggleClass("is__open");
+});
 
-// //sticky header
-// $(function () {
-//   // ページ下部固定ボタン表示非表示
-//   $(window).on("load scroll", function () {
-//     $header = $(".p-header");
-//     btnOffset = $(".l-cont--contact").offset().top;
-//     x = $(".p-header").height();
-//     fixedClass = "p-header--fixed";
-//   });
-//   $(function () {
-//     $(window).scroll(function () {
-//       if ($(this).scrollTop() > btnOffset - x) {
-//         $header.addClass(fixedClass);
-//       } else {
-//         $header.removeClass(fixedClass);
-//       }
-//     });
-//   });
-// });
+$(".p-burger-view__close p").on("click", function () {
+  $("body").toggleClass("is__fixed");
+  // $(".p-header__burger").toggleClass("is__active");
+  $(".p-burger-view").toggleClass("is__open");
+});
