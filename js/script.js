5aa26c8c0d968e015fcdf831fa716a5db443825a
sora-web <brave27yuto@gmail.com>
2022-09-17 20:47:49 +0900

    修正　アニメーション追加

--- js/script.js
+++ js/script.js
@@ -10,3 +10,10 @@ $(".p-burger-view__close p").on("click", function () {
   // $(".p-header__burger").toggleClass("is__active");
   $(".p-burger-view").toggleClass("is__open");
 });
+
+AOS.init({
+  offset: 100,
+  delay: 100,
+  duration: 500,
+  easing: "ease-in",
+});
