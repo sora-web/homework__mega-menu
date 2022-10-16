57f0d3da7d082940bd6c517d7edf364719758a81
sora-web <brave27yuto@gmail.com>
2022-10-16 11:17:14 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -3,23 +3,53 @@ window.onload = function () {
   const preload = document.getElementsByClassName("preload");
   preload[0].classList.remove("preload");
 };
+
+//
 //ハンバーガーメニュー
-$(".c-burger").on("click", function () {
-  $("body").toggleClass("is__fixed");
-  $(".c-burger").toggleClass("is__active");
-  $(".c-burger-view").toggleClass("is__open");
+//
+const burger = document.querySelector(".c-burger");
+const burgerView = document.querySelector(".c-burger-view");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("is__active");
+  burgerView.classList.toggle("is__open");
 });
 
-//contentsにマウスを移動したとき、矢印を固定する記述
-
+//
+//矢印
+//
 const contents = document.querySelectorAll(".p-header-contents");
 const arrows = document.querySelectorAll(".p-header__item--arrow a");
+const inner = document.querySelectorAll(".p-header-contents__inner");
 
+//itemにマウスを移動したとき、矢印を固定する記述
+for (let i = 0; i < arrows.length; i++) {
+  arrows[i].addEventListener("mouseover", () => {
+    contents[i].classList.add("is__visible");
+  });
+  arrows[i].addEventListener("mouseleave", () => {
+    contents[i].classList.remove("is__visible");
+  });
+}
+
+//contentsにマウスを移動したとき、矢印を固定する記述
 for (let i = 0; i < contents.length; i++) {
   contents[i].addEventListener("mouseover", () => {
     arrows[i].classList.add("is__hover");
   });
   contents[i].addEventListener("mouseleave", () => {
     arrows[i].classList.remove("is__hover");
+  });
+}
+
+//contentsにマウスを移動したとき、innerを固定する記述
+for (let i = 0; i < contents.length; i++) {
+  inner[i].addEventListener("mouseover", () => {
+    inner[i].classList.add("is__stay");
+    contents[i].classList.add("is__stay");
+  });
+  inner[i].addEventListener("mouseleave", () => {
+    inner[i].classList.remove("is__stay");
+    contents[i].classList.remove("is__stay");
   });
 }
